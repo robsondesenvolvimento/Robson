34b78eb3799789e359a1c0da852c4fbac9713d2c
Robson Candido dos Santos Alves <robsondesenvolvimento@gmail.com>
2020-10-22 23:54:27 -0300

    Iniciando projeto nos principios DDD, SOLID, TDD

---
+++ Robson.Domain/Entities/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Robson.Domain.Entities
+{
+    public class Pessoa
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public DateTime Nascimento { get; private set; }
+
+        public Pessoa(int id, string nome, DateTime nascimento)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Nascimento = nascimento;
+        }
+    }
+}
---
+++ Robson.Testes/PessoaTeste.cs
@@ -0,0 +1,25 @@
+﻿using Robson.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Robson.Testes
+{
+    public class PessoaTeste
+    {
+        public static readonly object[][] Pessoa =
+        {
+            new object[] { 1, "Robson Candido dos Santos ALves", DateTime.Parse("1980-08-29") }
+        };
+
+        [Theory]
+        [MemberData(nameof(Pessoa))]
+        public void CriarPessoa(int id, string nome, DateTime nascimento)
+        {
+            Pessoa pessoa = new Pessoa(id, nome, nascimento);
+            var idade = DateTime.Parse("2020-10-22").Year - pessoa.Nascimento.Year;
+            Assert.Equal(40, idade);
+        }
+    }
+}
